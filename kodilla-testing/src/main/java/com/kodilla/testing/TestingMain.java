41eed7931b08ead09d625bdbcbe8bb55d0322eaa
AnnaLurka <annaklurka@gmail.com>
2019-09-18 09:16:54 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,41 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-
     public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        System.out.println("Moduł 6. Wprowadzenie do testowwania oprogramowania");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("Test Ok.");
+        } else {
+            System.out.println("Error");
+        }
+
+
+        Calculator calculator = new Calculator(2,3);
+
+        int result1 = calculator.add();
+
+        if(result1 == 5){
+            System.out.println("Test Ok.");
+        } else {
+            System.out.println("Error");
+        }
+
+        int result2 = calculator.substract();
+
+        if(result2 == -1){
+            System.out.println("Test Ok.");
+        } else {
+            System.out.println("Error");
+        }
+
+
+
+
     }
 }
