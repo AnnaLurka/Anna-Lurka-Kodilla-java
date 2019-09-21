21b1384b302e47726084b62895febafc1c0583ab
AnnaLurka <annaklurka@gmail.com>
2019-09-21 07:03:32 +0200

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -7,10 +7,6 @@ public class OddNumbersExterminator {
     private ArrayList<Integer> numbers;
     private ArrayList<Integer> even;
 
-    public OddNumbersExterminator(ArrayList<Integer> numbers, ArrayList<Integer> even ) {
-        this.numbers = numbers;
-        this.even = even;
-    }
 
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
 
