2831f7f160c2463b9185598c6c7fc0a33b358395
AnnaLurka <annaklurka@gmail.com>
2019-09-21 07:42:24 +0200

    6.4

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumPost.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumPost.java
@@ -9,4 +9,30 @@ public class ForumPost {
         this.postBody = postBody;
         this.author = author;
     }
+
+    public String getPostBody() {
+        return postBody;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ForumPost forumPost = (ForumPost) o;
+
+        if (!postBody.equals(forumPost.postBody)) return false;
+        return author.equals(forumPost.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = postBody.hashCode();
+        result = 31 * result + author.hashCode();
+        return result;
+    }
 }
