4e6947c5466fd1bf207815626574470ec7cbea30
AnnaLurka <annaklurka@gmail.com>
2019-09-27 18:57:58 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -6,6 +6,9 @@ import java.io.*;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 
 import java.awt.Font;
@@ -14,6 +17,21 @@ import java.util.StringJoiner;
 public class StreamMain {
     public static void main(String[] args) {
 
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
@@ -21,6 +39,9 @@ public class StreamMain {
         poemBeautifier.beautify("beauty", (text) -> "***" + text + "***");
         poemBeautifier.beautify("beauty", (text) -> String.join("-", text, text));
         poemBeautifier.beautify("beauty", (text) -> "<" + text + ">");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
 
     }
