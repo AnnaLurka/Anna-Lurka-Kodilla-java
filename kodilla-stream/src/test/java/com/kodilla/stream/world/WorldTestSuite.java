1bfd09c67296d2c51a14dc8168b5cc6d0bebae9f
AnnaLurka <annaklurka@gmail.com>
2019-09-30 23:09:05 +0200

    7.4

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -11,18 +11,25 @@ public class WorldTestSuite {
     @Test
     public void testGetPeopleQuantity() {
 
+        List<Country> europe = new ArrayList<>();
+        europe.add(new Country("poland", new BigDecimal("37000000")));
+        europe.add(new Country("germany", new BigDecimal("82000000")));
+        europe.add(new Country("slovakia", new BigDecimal("5000000")));
+
+        List<Country> africa = new ArrayList<>();
+        africa.add(new Country("southAfrica", new BigDecimal("58000000")));
+        africa.add(new Country("egypt", new BigDecimal("101000000")));
+        africa.add(new Country("nigeria", new BigDecimal("201000000")));
+
+        List<Country> asia = new ArrayList<>();
+        asia.add(new Country("china", new BigDecimal("1393000000")));
+        asia.add(new Country("india", new BigDecimal("1369000000")));
+        asia.add(new Country("japan", new BigDecimal("126000000")));
+
         List<Continent> continents = new ArrayList<>();
-
-
-        continents.add(new Country("poland", new BigDecimal("37000000")));
-        continents.add(new Country("germany", new BigDecimal("82000000")));
-        continents.add(new Country("slovakia", new BigDecimal("5000000")));
-        continents.add(new Country("southAfrica", new BigDecimal("58000000")));
-        continents.add(new Country("egypt", new BigDecimal("101000000")));
-        continents.add(new Country("nigeria", new BigDecimal("201000000")));
-        continents.add(new Country("china", new BigDecimal("1393000000")));
-        continents.add(new Country("india", new BigDecimal("1369000000")));
-        continents.add(new Country("japan", new BigDecimal("126000000")));
+        continents.add(new Continent(europe));
+        continents.add(new Continent(africa));
+        continents.add(new Continent(asia));
 
         //When
         World world = new World();
