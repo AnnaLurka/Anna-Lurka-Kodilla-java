430400b795ae0acd0f23d4bb5342f70cf5a655a6
AnnaLurka <annaklurka@gmail.com>
2019-10-06 17:30:16 +0200

    8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,49 @@
+package com.kodilla.exception.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightRunner {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException{
+
+
+        Map<String, Boolean> flightMap = new HashMap<>();
+        flightMap.put("Cracow", true);
+        flightMap.put("Warsaw", true);
+        flightMap.put("Gdańsk", false);
+        flightMap.put("Poznań", true);
+        flightMap.put("Rzeszów", false);
+
+
+        if(flightMap.containsKey(flight.getArrivalAirport())) {
+            if (flightMap.get(flight.getArrivalAirport())) {
+                System.out.println("There is a connection.");
+
+            } else {
+                System.out.println("There is no connection.");
+            }
+
+        } else {
+            throw new RouteNotFoundException("Route not found.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FlightRunner flightRunner = new FlightRunner();
+
+        try {
+            flightRunner.findFlight(new Flight("Cracow" , "Warsaw"));
+            flightRunner.findFlight(new Flight("Cracow" , "Rzeszów"));
+            flightRunner.findFlight(new Flight("Cracow" , "Wieliczka"));
+
+        } catch (RouteNotFoundException e) {
+            System.out.println("There is no such an airport.");
+
+        } finally{
+            System.out.println("The end of search.");
+        }
+    }
+}
