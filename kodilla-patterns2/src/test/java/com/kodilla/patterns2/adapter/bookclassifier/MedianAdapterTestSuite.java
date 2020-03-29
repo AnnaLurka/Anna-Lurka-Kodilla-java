bfbb7d6531745424ab6407367545ad162a046d6f
AnnaLurka <annaklurka@gmail.com>
2020-03-29 18:13:54 +0200

    24.5

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -11,24 +11,44 @@ import static org.junit.Assert.assertEquals;
 
 public class MedianAdapterTestSuite {
     @Test
-    public void publicationYearMedianTest() {
+    public void publicationYearMedianTestWithOddSize() {
         //Given
         Book book1 = new Book("Author1", "Title1", 2001, "001");
         Book book2 = new Book("Author2", "Title2", 2002, "002");
         Book book3 = new Book("Author3", "Title3", 2003, "003");
         Book book4 = new Book("Author4", "Title4", 2004, "004");
-        //Book book5 = new Book("Author5", "Title5", 2005, "005");
+        Book book5 = new Book("Author5", "Title5", 2005, "005");
         Set<Book> librarya = new HashSet<>();
         librarya.add(book1);
         librarya.add(book2);
         librarya.add(book3);
         librarya.add(book4);
-        //librarya.add(book5);
+        librarya.add(book5);
         MedianAdapter adapter = new MedianAdapter();
         //When
         int result = adapter.publicationYearMedian(librarya);
         //Then
         System.out.println(result);
-        assertEquals(result, 2002.5, 0);
+        assertEquals(2003, result, 0);
+    }
+
+    @Test
+    public void publicationYearMedianTestWithEvenSize() {
+        //Given
+        Book book1 = new Book("Author1", "Title1", 2001, "001");
+        Book book2 = new Book("Author2", "Title2", 2002, "002");
+        Book book3 = new Book("Author3", "Title3", 2003, "003");
+        Book book4 = new Book("Author4", "Title4", 2004, "004");
+        Set<Book> librarya = new HashSet<>();
+        librarya.add(book1);
+        librarya.add(book2);
+        librarya.add(book3);
+        librarya.add(book4);
+        MedianAdapter adapter = new MedianAdapter();
+        //When
+        int result = adapter.publicationYearMedian(librarya);
+        //Then
+        System.out.println(result);
+        assertEquals(2003, result);
     }
 }
